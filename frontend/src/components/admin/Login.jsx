d750d66ce792074b8bd399c23d21621f918df642
oganand <anandog12@gmail.com>
2026-03-09 11:01:23 +0530

    added login url

--- frontend/src/components/admin/Login.jsx
+++ frontend/src/components/admin/Login.jsx
@@ -5,6 +5,7 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+  
   const API_URL = process.env.REACT_APP_API_URL;
 
 
