cfbcd5ba0a04948dc58762c64ccfb9395e4e3de1
oganand <anandog12@gmail.com>
2026-03-22 20:43:20 +0530

    Product page changed to fetch API

--- frontend/src/components/user/ProductDetails.jsx
+++ frontend/src/components/user/ProductDetails.jsx
@@ -1,46 +1,52 @@
 import { useParams, useNavigate } from "react-router-dom";
-import { products } from "./data/products";
+import { useEffect, useState } from "react";
 import "../styles/explore.css";
 
 function ProductDetails() {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  const product = products.find((p) => p.id.toString() === id);
+  const [product, setProduct] = useState(null);
 
-  if (!product) return <h2 className="not-found">Product not found</h2>;
+  useEffect(() => {
+    fetch(`https://sareewebsite.onrender.com/sarees`)
+      .then((res) => res.json())
+      .then((data) => {
+        const item = data.find((p) => p._id === id);
+
+        if (item) {
+          setProduct({
+            id: item._id,
+            name: item.name,
+            price: item.price,
+            image: item.image,
+            color: item.color,
+            fabric: item.material,
+            description: item.sareeType,
+          });
+        }
+      });
+  }, [id]);
+
+  if (!product) return <p>Loading...</p>;
 
   return (
     <div className="explore-app">
       <div className="product-details-container">
-        
-        {/* Back Button */}
         <button className="back-btn" onClick={() => navigate("/explore")}>
-          ← Back to Explore
+          ← Back
         </button>
 
         <div className="product-details">
-          
-          {/* Image */}
           <div className="details-image">
             <img src={product.image} alt={product.name} />
           </div>
 
-          {/* Info */}
           <div className="details-info">
             <h1 className="details-title">{product.name}</h1>
-
-            <p className="details-meta">
-              {product.fabric} • {product.color}
-            </p>
-
-            <p className="details-description">
-              {product.description}
-            </p>
-
+            <p>{product.fabric} • {product.color}</p>
+            <p className="details-description">{product.description}</p>
             <h2 className="details-price">₹ {product.price}</h2>
-
-            <button className="buy-btn">Add to Cart</button>
           </div>
         </div>
       </div>
